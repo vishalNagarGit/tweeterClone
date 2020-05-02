01027946b0315d1cd5bba0923011460903c1a080
vishalNagarGit <vishaln@mnnit.ac.in>
2020-05-02 23:06:58 +0530

    login signup created

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,19 +9,72 @@
 package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class  MainActivity extends AppCompatActivity {
+
+   EditText username;
+  EditText passwd;
+
+  public void loginMethod(View view)
+  {
+    if(username.getText()==null||passwd.getText().toString()==null)
+      Toast.makeText(this, "invalid username or passwd", Toast.LENGTH_SHORT).show();
+    else
+    ParseUser.logInInBackground(username.getText().toString(), passwd.getText().toString(), new LogInCallback() {
+      @Override
+      public void done(ParseUser user, ParseException e) {
+         if(e==null)
+         {
+           Log.i("login","success");
+         }
+        else{
+           Toast.makeText(MainActivity.this, "could not log in", Toast.LENGTH_SHORT).show();
+         }
+
+      }
+    });
+  }
+
+  public void signupMethod(View view)
+  { if(username.getText()==null||passwd.getText().toString()==null)
+    Toast.makeText(this, "invalid username or passwd", Toast.LENGTH_SHORT).show();
+
+   else
+      {
+         ParseUser user=new ParseUser();
+         user.put("username",username.getText().toString());
+         user.put("password",passwd.getText().toString());
+        user.signUpInBackground(new SignUpCallback() {
+          @Override
+          public void done(ParseException e) {
+            if(e==null)
+              Toast.makeText(MainActivity.this, "signup success", Toast.LENGTH_SHORT).show();
+            else
+              Toast.makeText(MainActivity.this, "could not signup", Toast.LENGTH_SHORT).show();
+          }
+        });
+      }
+  }
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-     //testLogin
-
+     setTitle("Tweeter Login/signup");
+    username=(EditText)findViewById(R.id.userNameEditText);
+    passwd=(EditText)findViewById(R.id.passwdEditText);
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
 
